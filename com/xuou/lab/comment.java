ea6c58b51abac6047002729c4a5d685ba2e4874a
slidexuou <slidexuou7@gmail.com>
2021-03-31 10:23:05 +0700

    2.1

---
+++ com/xuou/lab/comment.java
@@ -0,0 +1,25 @@
+package com.xuou.lab;
+
+public class comment {
+	public static void main(String[] args) {
+//	int i;
+		/*
+		 * int j; int k;
+		 */
+		int myInt;
+		myInt = 20;
+		System.out.println("myInt value " + myInt);
+		boolean myStatus;
+		myStatus = true;
+		System.out.println("isSingle " + myStatus);
+		String myMind;
+		myMind = "Hello World";
+		System.out.println("This is String " + myMind);
+		float myFloat;
+		myFloat = 0.0f;
+		System.out.println("My float is " + myFloat);
+		byte myByte;
+		myByte = 1;
+		System.out.println("My byte is " + myByte);
+	}
+}
